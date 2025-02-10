0262a8e5b346df2d56cd418a92d53e3ebd2b503a
Agostina Gil <agostinagil973@gmail.com>
2025-02-10 14:30:54 +0100

    feat: add Sale section to SingleBook view

--- src/views/SingleBook/SingleBook.jsx
+++ src/views/SingleBook/SingleBook.jsx
@@ -9,6 +9,10 @@ const SingleBook = () => {
   const { id } = useParams();
   const plainText = data[0]?.volumeInfo?.description.replace(/<[^>]*>?/gm, "");
   const info = data[0]?.volumeInfo;
+  const price = data[0]?.saleInfo?.listPrice?.amount;
+  const currency = data[0]?.saleInfo?.listPrice?.currencyCode;
+  const buyLink = data[0]?.saleInfo?.buyLink;
+  const previewLink = data[0]?.accessInfo?.webReaderLink;
 
   useEffect(() => {
     if (!id) {
@@ -26,6 +30,7 @@ const SingleBook = () => {
         {error && <p>{error}</p>}
         {data.length > 0 && info ? (
           <main className="pt-24">
+            {/* Book Cover */}
             <div className="h-[375px] w-11/12 mx-auto rounded-lg bg-white self-center flex items-center justify-center">
               <img
                 src={info.imageLinks.smallThumbnail}
@@ -44,12 +49,16 @@ const SingleBook = () => {
                 <p>Unknown author</p>
               )}
 
+              {/* Book's title and subtitle */}
+
               <h1 className="text-[24px] text-title-black font-bold mt-5 mb-2 cursor-default">
                 {info.title}
               </h1>
               <span className="text-base">{info.subtitle}</span>
 
+              {/* Description*/}
               <Divisor />
+
               <div>
                 <h2 className="text-2xl font-semibold mb-4 text-subtitle-black">
                   Description
@@ -57,23 +66,82 @@ const SingleBook = () => {
                 {<BookDescription description={plainText} />}
               </div>
 
+              {/* Book's sale info */}
+
               <Divisor />
+              <div>
+                <h3 className="text-xl font-semibold mb-4 text-subtitle-black">
+                  Do you want to read it?
+                </h3>
+                <div className="flex text-sm mb-1">
+                  <p className="block mr-3">
+                    <b>Price:</b>
+                  </p>
+                  <p className="block">
+                    {price && currency
+                      ? `${price} ${currency}`
+                      : "Unknown price"}
+                  </p>
+                </div>
+                <div className="flex text-sm mb-1">
+                  <p className="block mr-3">
+                    <b>Availability:</b>
+                  </p>
+                  <p className="block">
+                    {buyLink ? (
+                      <a
+                        href={buyLink}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="transition-all duration-300 hover:text-main-color"
+                      >
+                        Buy here
+                      </a>
+                    ) : (
+                      "Not available"
+                    )}
+                  </p>
+                </div>
+                <div className="flex text-sm">
+                  <p className="block mr-3">
+                    <b>Preview:</b>
+                  </p>
+                  <p className="block">
+                    {previewLink ? (
+                      <a
+                        href={previewLink}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="transition-all duration-300 hover:text-main-color"
+                      >
+                        Preview Link
+                      </a>
+                    ) : (
+                      "Link not available"
+                    )}
+                  </p>
+                </div>
+              </div>
+
+              {/* Book's details */}
+              <Divisor />
+
               <div className="text-sm">
                 <h3 className="text-xl font-semibold mb-4 text-title-black">
                   Details
                 </h3>
-                <div className="flex ">
-                  <p className="block mr-5">
-                    <b>Publisher</b>
+                <div className="flex mb-1">
+                  <p className="block mr-3">
+                    <b>Publisher:</b>
                   </p>
 
                   <p className="block">
                     {info.publisher ? info.publisher : "Unknown publisher"}
                   </p>
                 </div>
-                <div className="flex">
-                  <p className="block mr-5">
-                    <b>Publish date</b>
+                <div className="flex mb-1">
+                  <p className="block mr-3">
+                    <b>Publish date:</b>
                   </p>
                   <p className="block">
                     {info.publishedDate
@@ -81,9 +149,9 @@ const SingleBook = () => {
                       : "Unkwnown publish date"}
                   </p>
                 </div>
-                <div className="flex">
-                  <p className="block mr-5">
-                    <b>Categories</b>
+                <div className="flex mb-1">
+                  <p className="block mr-3">
+                    <b>Categories:</b>
                   </p>
 
                   <p className="block">
